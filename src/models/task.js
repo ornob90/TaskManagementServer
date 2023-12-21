04d7477b1d92760c99411ce21aba4a3175da719d
ornob90 <oornob49@gmail.com>
2023-12-21 21:23:32 +0600

    task model complete

---
+++ src/models/task.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  deadline: {
+    type: Date,
+    required: true,
+  },
+  priority: {
+    type: String,
+    default: 'moderate',
+  },
+  status: {
+    type: String
+    default: 'to-do',
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
